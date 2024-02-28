038359a2d572207a3541b1e91510c16273ac441e
Hamidur Rashid <hamidmonju01@gmail.com>
2024-02-28 17:45:43 +0600

    learn react router dom wiht official documentation and explore it in this react router dom tutorial

---
+++ src/actions/contactsActions.js
@@ -0,0 +1,20 @@
+import { redirect } from "react-router-dom";
+import { createContact, deleteContact, updateContact } from "../contacts";
+
+export async function createContactAction() {
+  const contact = await createContact();
+  return { contact };
+}
+
+export async function editContactAction({ request, params }) {
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData);
+  console.log(updates);
+  await updateContact(params.contactId, updates);
+  return redirect(`/contacts/${params.contactId}`);
+}
+
+export async function deleteContactAction({ params }) {
+  await deleteContact(params.contactId);
+  return redirect("/");
+}
---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Contact from "./Contact";
+import EditContact from "./EditContact";
+import ErrorPage from "./Error";
+import Root from "./Root";
+import {
+  createContactAction,
+  deleteContactAction,
+  editContactAction,
+} from "./actions/contactsActions";
+import "./index.css";
+import { getContactLoader, getContactsLoader } from "./loaders/contactsLoader";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: getContactsLoader,
+    action: createContactAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: getContactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: getContactLoader,
+        action: editContactAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: deleteContactAction,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
